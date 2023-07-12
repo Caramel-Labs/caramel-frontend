01fb6bceb6d4e4cbbb463672854add0f13f2a2a1
Manujaya <73481420+Iam-SilentEyes@users.noreply.github.com>
2023-07-12 18:18:51 +0530

    changed routing

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -6,12 +6,13 @@ import { usePathname } from 'next/navigation'
 const Navbar = () => {
 
    const Menus = [
-      {name:"Home", route:"/"},
+      {name:"Home", route:"/home"},
       {name:"Explore", route:"/explore"},
       {name:"Profile", route:"/profile"}
    ]
    
    const pathName = usePathname();
+
 
    return (
       <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
@@ -19,7 +20,7 @@ const Navbar = () => {
 
       {Menus.map((menu, i)=>(
 
-         <Link key= {i} href={menu.route} className= {`inline-flex flex-col items-center justify-center font-medium px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group ${pathName === menu.route && " bg-blue-600"}`}>
+         <Link key= {i} href={menu.route} className= {`inline-flex flex-col items-center justify-center font-medium px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group ${pathName.includes(menu.route) && " bg-blue-600"}`}>
             <button  type="button">
                <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">{menu.name}</span>
             </button>
