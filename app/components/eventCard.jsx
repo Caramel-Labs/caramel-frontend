9ad4c39f2e88fa550d0d04df662a217a30d88e17
Manujaya <73481420+Iam-SilentEyes@users.noreply.github.com>
2023-07-16 00:37:22 +0530

    Home page + event card comp is done හුත්තො

---
+++ app/components/eventCard.jsx
@@ -0,0 +1,22 @@
+'use client'
+import Image from 'next/image'
+
+export default function EventCard(props){
+
+    const {title, society, i} = props
+     
+    return (
+        <div key={i}className="bg-white relative shadow-lg hover:shadow-xl transition duration-500 rounded-lg border-black border-2">
+            <Image className = "rounded-t-lg " src="/../public/step.jpg" alt="" width= "390" height="200"/>
+            <div className="py-6 px-8 rounded-lg bg-white">
+              <h1 className="text-gray-700 font-bold text-xl mb-3 hover:text-gray-900 hover:cursor-pointer">{title ||eventName}</h1>
+              <p className="text-gray-700 tracking-wide">{society}</p>
+              <button className="mt-6 py-2 px-4 bg-orange-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300"> Join Now </button>
+            </div>
+            <div className="absolute top-2 right-2 py-2 px-4 bg-black rounded-lg">
+              <span className="text-md text-white"> 2 days more </span>
+            </div>
+          </div>
+
+    )
+}
--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,23 +1,19 @@
-import Image from "next/image"
+import getData from '@/app/utility/getData.js'
+import EventCard from '@/app/components/eventCard.jsx'
 
-export default function Home() {
+export default async function Home() {
+  
+ const events  = await getData('http://localhost:3001/events/all')
+ //console.log(events)
   return (
   <div>
     
     <h1 className="text-gray-700 font-bold text-2xl">Events</h1> 
-        
-        {/* The events card component */}
-          <div className="bg-white relative shadow-lg hover:shadow-xl transition duration-500 rounded-lg border-black border-2">
-            <Image className = "rounded-t-lg " src="/../public/step.jpg" alt="" width= "390" height="200"/>
-            <div className="py-6 px-8 rounded-lg bg-white">
-              <h1 className="text-gray-700 font-bold text-xl mb-3 hover:text-gray-900 hover:cursor-pointer">Step into Industry</h1>
-              <p className="text-gray-700 tracking-wide">IEEE Student branch</p>
-              <button className="mt-6 py-2 px-4 bg-orange-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300"> Join Now </button>
-            </div>
-            <div className="absolute top-2 right-2 py-2 px-4 bg-black rounded-lg">
-              <span className="text-md text-white"> 2 days more </span>
-            </div>
-          </div>
+    
+        {events.map((event, i)=>(
+          <EventCard key={i} title={event.title || event.eventName} society={event.society} />
+         ))}
+          
 
   </div>
   )
