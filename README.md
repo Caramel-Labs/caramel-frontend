4a14301a13b114afae712dfbaddd15c3f542dad4
Manujaya <73481420+Iam-SilentEyes@users.noreply.github.com>
2023-07-17 01:51:15 +0530

    join btn toggling added

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 - Map the events in the Profile page using data from the received JSON.<br>
 - The profile button in the navbar should be routed to the current user's profile.<br>
+- Use ISR to update the content in the feed with a specific time interval (60s?) <br>
+
+# Disqus
+
+- Debouncing || Batching calls in the feed (join button) ??
--- app/components/eventCard.jsx
+++ app/components/eventCard.jsx
@@ -1,9 +1,11 @@
 'use client'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function EventCard(props) {
 
     const {title, eventName, society, i} = props
+    const [join, setJoin] = useState(false)
      
     return (
         <div key={i}className="bg-white relative shadow-lg hover:shadow-xl transition duration-500 rounded-lg border-black border-2 m-5">
@@ -13,7 +15,7 @@ export default function EventCard(props) {
                 <h1 className="text-gray-700 font-bold text-xl mb-3 hover:text-gray-900 hover:cursor-pointer mr-50 ml-50 mt-3">{title ||eventName}</h1>
                 <p className="text-gray-700 tracking-wide">{society}</p>
               </div>
-              <button className="mt-6 py-2 px-4 bg-orange-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300 ml-3 mb-6 mr-0"> Join </button>
+              <button className="mt-6 py-2 px-4 bg-orange-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300 ml-3 mb-6 mr-0" onClick={()=>{setJoin(!join)}}> {join ? "Joined": "Join"} </button>
             </div>
             <div className="absolute top-2 right-2 py-1.5 px-3 bg-black rounded-lg">
               <span className="text-md text-white text-xs"> 2 days more </span>
