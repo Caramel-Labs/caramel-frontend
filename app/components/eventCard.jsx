3f241ccd8934b82bb39f373233293915353073d0
Kalana Kaushalya <115646312+Kalana-Kaushalya@users.noreply.github.com>
2023-07-16 19:53:48 +0530

    Event Style

--- app/components/eventCard.jsx
+++ app/components/eventCard.jsx
@@ -6,12 +6,14 @@ export default function EventCard(props) {
     const {title, eventName, society, i} = props
      
     return (
-        <div key={i}className="bg-white relative shadow-lg hover:shadow-xl transition duration-500 rounded-lg border-black border-2">
+        <div key={i}className="bg-white relative shadow-lg hover:shadow-xl transition duration-500 rounded-lg border-black border-2 m-5">
             <Image className = "rounded-t-lg " src="/../public/step.jpg" alt="" width= "390" height="200"/>
-            <div className="py-6 px-8 rounded-lg bg-white">
-              <h1 className="text-gray-700 font-bold text-xl mb-3 hover:text-gray-900 hover:cursor-pointer">{title ||eventName}</h1>
-              <p className="text-gray-700 tracking-wide">{society}</p>
-              <button className="mt-6 py-2 px-4 bg-orange-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300"> Join Now </button>
+            <div className="py-6 px-8 rounded-lg bg-white flex justify-between">
+              <div>
+                <h1 className="text-gray-700 font-bold text-xl mb-3 hover:text-gray-900 hover:cursor-pointer mr-50 ml-50 mt-3">{title ||eventName}</h1>
+                <p className="text-gray-700 tracking-wide">{society}</p>
+              </div>
+              <button className="mt-6 py-2 px-4 bg-orange-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300 ml-3 mb-6 mr-0"> Join </button>
             </div>
             <div className="absolute top-2 right-2 py-2 px-4 bg-black rounded-lg">
               <span className="text-md text-white"> 2 days more </span>
--- app/home/page.jsx
+++ app/home/page.jsx
@@ -6,9 +6,9 @@ export default async function Home() {
  const events  = await getData('http://localhost:3001/events/all')
  //console.log(events)
   return (
-  <div>
+  <div className='p-5'>
     
-    <h1 className="text-gray-700 font-bold text-2xl">Events</h1> 
+    <h1 className="text-gray-700 font-bold text-2xl pb-3">Events</h1> 
     
         {events.map((event, i)=>(
           <EventCard key={i} title={event.title || event.eventName} society={event.society} />
