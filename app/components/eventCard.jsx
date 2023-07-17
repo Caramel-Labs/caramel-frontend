f5976c234111dabf8fbc4431e7834301846f8f12
Manujaya <73481420+Iam-SilentEyes@users.noreply.github.com>
2023-07-17 12:14:03 +0530

    gg

--- app/components/eventCard.jsx
+++ app/components/eventCard.jsx
@@ -13,7 +13,7 @@ export default function EventCard(props) {
             <Image className = "rounded-t-lg " src="/../public/step.jpg" alt="" width= "390" height="200"/>
             <div className="py-6 px-8 rounded-lg bg-white flex justify-between">
               <div>
-                <h1 className="text-gray-700 font-bold text-xl mb-3 hover:text-gray-900 hover:cursor-pointer mr-50 ml-50 mt-3">{title ||eventName}</h1>
+                <h1 className="text-gray-700 font-bold text-xl mb-3 hover:text-gray-900 hover:cursor-pointer mr-50 ml-50 mt-3">{eventName}</h1>
                 <p className="text-gray-700 tracking-wide">{society}</p>
               </div>
               <button className="mt-6 py-2 px-4 bg-orange-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300 ml-3 mb-6 mr-0" onClick={()=>{setJoin(true)}}> {join ? "Joined" : "Join"} </button>
