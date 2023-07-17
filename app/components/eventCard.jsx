c2047d99b3e91e96fee73ef11eaa3df1b7e5a036
ravi-aratchige <rmaratchige@gmail.com>
2023-07-17 22:28:41 +0530

    Minor changes to styles

--- app/components/eventCard.jsx
+++ app/components/eventCard.jsx
@@ -12,10 +12,10 @@ export default function EventCard(props) {
     return (
         <div key={i}className="bg-white relative drop-shadow-xl hover:shadow-xl transition duration-500 rounded-lg border-slate-300 border-1 my-5">
             <Image className = "rounded-t-lg " src="/../public/event-bg.jpg" alt="" width= "390" height="200"/>
-            <div className="py-3 px-4 rounded-lg bg-white flex justify-between">
+            <div className="py-1 px-4 rounded-lg bg-white flex justify-between">
               <div className='flex flex-col justify-center'>
-                <h1 className="text-gray-700 font-bold text-xl hover:text-gray-900 hover:cursor-pointer mr-50 ml-50 my-1">{eventName}</h1>
-                <p className="text-gray-700 tracking-wide">{society}</p>
+                <h1 className="text-gray-700 font-bold text-xl hover:text-gray-900 hover:cursor-pointer mr-50 ml-50 my-1 line-clamp-1">{eventName}</h1>
+                <p className="text-gray-700 tracking-wide text-sm">{society}</p>
               </div>
               <button className="py-2 px-4 bg-orange-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300 ml-3 my-6 mr-0 flex-shrink-0" onClick={()=>{setJoin(true)}}> {join ? "Joined" : "Join"} </button>
             </div>
