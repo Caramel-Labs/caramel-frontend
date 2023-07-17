7efb0868550120f0e3dc5ff08bcc00159d023621
ravi-aratchige <rmaratchige@gmail.com>
2023-07-17 22:16:46 +0530

    Fixed event card styling

--- app/components/eventCard.jsx
+++ app/components/eventCard.jsx
@@ -12,14 +12,14 @@ export default function EventCard(props) {
     return (
         <div key={i}className="bg-white relative drop-shadow-xl hover:shadow-xl transition duration-500 rounded-lg border-slate-300 border-1 my-5">
             <Image className = "rounded-t-lg " src="/../public/event-bg.jpg" alt="" width= "390" height="200"/>
-            <div className="py-6 px-8 rounded-lg bg-white flex justify-between">
-              <div>
-                <h1 className="text-gray-700 font-bold text-xl mb-3 hover:text-gray-900 hover:cursor-pointer mr-50 ml-50 mt-3">{eventName}</h1>
+            <div className="py-3 px-4 rounded-lg bg-white flex justify-between">
+              <div className='flex flex-col justify-center'>
+                <h1 className="text-gray-700 font-bold text-xl hover:text-gray-900 hover:cursor-pointer mr-50 ml-50 my-1">{eventName}</h1>
                 <p className="text-gray-700 tracking-wide">{society}</p>
               </div>
-              <button className="mt-6 py-2 px-4 bg-orange-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300 ml-3 mb-6 mr-0" onClick={()=>{setJoin(true)}}> {join ? "Joined" : "Join"} </button>
+              <button className="py-2 px-4 bg-orange-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300 ml-3 my-6 mr-0 flex-shrink-0" onClick={()=>{setJoin(true)}}> {join ? "Joined" : "Join"} </button>
             </div>
-            <div className="absolute top-2 right-2 py-1.5 px-3 bg-black rounded-lg">
+            <div className="absolute top-4 right-4 py-1.5 px-3 bg-black rounded-lg">
               <span className="text-md text-white text-xs">2 days more</span>
             </div>
           </div>
--- app/page.jsx
+++ app/page.jsx
@@ -5,8 +5,7 @@ export default function rickRoll() {
   return(
    <div className="grid h-screen place-items-center">
     <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank"> 
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> මෙතන ඔබන්න
-  </button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">මෙතන ඔබන්න</button>
     </Link>
   </div>  
   )
