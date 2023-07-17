15c714fe26331567d244cae4179e5e1fa9695ec6
ravi-aratchige <rmaratchige@gmail.com>
2023-07-17 22:20:40 +0530

    Minor changes to heading

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -7,13 +7,13 @@ export default async function Home() {
   
  const events  = await getData('http://localhost:3001/events/all')
  const today = getToday()
- //console.log(today)
+ // console.log(today)
 
   return (
   <div className='p-5'>
     
     <h3 className='font-bold'> {today} </h3>
-    <h1 className="font-bold text-2xl pb-3">Events</h1> 
+    <h1 className="font-bold text-3xl pb-3">EVENTS</h1> 
     
         {events.map((event, i)=>(
           <EventCard key={i} eventName={event.eventName} society={event.society} />
