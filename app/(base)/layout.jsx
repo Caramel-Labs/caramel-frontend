bd620f554a5e72bd99eec3a5a632b38ea68c33e0
Manujaya <73481420+Iam-SilentEyes@users.noreply.github.com>
2023-07-22 02:29:20 +0530

    changed routing group

--- app/(base)/layout.jsx
+++ app/(base)/layout.jsx
@@ -1,5 +1,6 @@
-import './globals.css'
+import '@/app/globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/app/components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Navbar/>
       </body>
     </html>
   )
