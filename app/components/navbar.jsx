9b47764242099fda3e05b8d7f2758ba2424f7e5f
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-07-21 22:04:49 +0530

    Added default user @lasindu-ranasinghe to navbar route

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
    const Menus = [
       {name:"Home", route:"/home"},
       {name:"Explore", route:"/explore"},
-      {name:"Profile", route:"/user"}
+      {name:"Profile", route:"/user/lasindu2001"}
    ]
    
    const pathName = usePathname();
@@ -16,20 +16,20 @@ const Navbar = () => {
 
    return (
       <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
-      <div className="grid h-full max-w-lg grid-cols-3 mx-auto">
+         <div className="grid h-full max-w-lg grid-cols-3 mx-auto">
 
-      {Menus.map((menu, i)=>(
+            {Menus.map((menu, i)=>(
 
-         <Link key={i} href={menu.route} className={`inline-flex flex-col items-center justify-center font-medium px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group ${pathName.includes(menu.route) && " bg-blue-600"}`}>
-            <button type="button">
-               <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">{menu.name}</span>
-            </button>
-         </Link>
-      
-      ))}
+               <Link key={i} href={menu.route} className={`inline-flex flex-col items-center justify-center font-medium px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group ${pathName.includes(menu.route) && " bg-blue-600"}`}>
+                  <button type="button">
+                     <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">{menu.name}</span>
+                  </button>
+               </Link>
+            
+            ))}
 
+         </div>
       </div>
-  </div>
    )
 }
 
