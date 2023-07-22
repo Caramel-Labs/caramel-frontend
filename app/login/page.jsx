99eaa9ebe1843e4eb763cd1a407e7170232cb7c8
ravi-aratchige <rmaratchige@gmail.com>
2023-07-22 21:28:38 +0530

    Styled login page properly

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,34 +1,36 @@
 import Link from "next/link"
 
-export default function LoginPage(){
+export default function LoginPage() {
     return (
-        <div className="max-w-2xl mx-auto">
-            <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-               
-                <form className="space-y-6" action="#">
-                    
-                    <h3 className="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
-                    <div>
-                        <label for="email" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Your email</label>
-                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required=""/>
-                    </div>
+        <div className="h-screen relative flex content-center justify-center">
+            <div className="flex flex-col content-center justify-center">
+                <div className="bg-white rounded-lg h-1/2 max-w-sm p-4 sm:p-6 lg:p-8">
+                
+                    <form className="space-y-6" action="#">
+                        
+                        <h3 className="text-3xl font-extrabold">SIGN IN TO CARAMEL</h3>
+                        <div>
+                            <label for="email" className="text-sm font-medium block mb-2">Your email</label>
+                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5" placeholder="name@example.com" required=""/>
+                        </div>
 
-                    <div>
-                         <label for="password" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Your password</label>
-                         <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required=""/>
-                    </div>
+                        <div>
+                            <label for="password" className="text-sm font-medium block mb-2">Your password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5" required=""/>
+                        </div>
 
-                    <div className="flex items-start">
-                        <Link href="#" className="text-sm text-blue-700 hover:underline ml-auto dark:text-blue-500">forgot Password? </Link>
-                    </div>
+                        <div className="flex items-start">
+                            <Link href="#" className="text-sm text-orange-700 hover:underline ml-auto dark:text-orange-500">Forgot password?</Link>
+                        </div>
 
-                    <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-                    
-                    <div className="text-sm font-medium text-gray-500 dark:text-gray-300"> Not registered? 
-                         <Link href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</Link>
-                    </div>
-                    
-                </form>
+                        <button type="submit" className="w-full text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">Login to your account</button>
+                        
+                        <div className="text-sm font-medium text-gray-500">Not registered?&nbsp;
+                            <Link href="#" class="text-orange-700 hover:underline dark:text-orange-500">Create account</Link>
+                        </div>
+                        
+                    </form>
+                </div>
             </div>
         </div>
     )
