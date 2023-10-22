0216d6fd8e3542b4ea8463c7d50abb82836c904f
Kalana Kaushalya <115646312+Kalana-Kaushalya@users.noreply.github.com>
2023-10-22 23:12:17 +0530

    style user page

--- app/components/eventList.jsx
+++ app/components/eventList.jsx
@@ -18,8 +18,8 @@ export default function EventList(props) {
 
 	return joinedEvents.map((event, i) => (
 		<Link key={i} href={`../event/${event._id}`}>
-			<li key={i} className="pb-3 sm:pb-4">
-				<div className="flex items-center space-x-4">
+			<li key={i} className="py-3 sm:py-4">
+				<div className="flex items-center space-x-4 pb-2">
 					<div className="flex-shrink-0">
 						<Image
 							className="rounded-full"
@@ -30,16 +30,17 @@ export default function EventList(props) {
 						/>
 					</div>
 					<div className="flex-1 min-w-0">
-						<p className="text-sm font-medium text-black truncate">
+						<p className="text-xs font-medium text-white truncate">
 							{event.name}
 						</p>
-						<p className="text-sm text-black truncate">{event.society}</p>
+						<p className="text-[10px] text-white truncate">{event.society}</p>
 					</div>
-					<div className="inline-flex items-center text-base font-semibold text-black">
+					<div className="inline-flex items-center font-semibold text-white text-xs">
 						{handleDaysLeft(event.date, event.time)} <br />
 						more
 					</div>
 				</div>
+				<hr className="h-px border-gray-700" />
 			</li>
 		</Link>
 	));
