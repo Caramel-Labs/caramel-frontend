d5b985e298bbcfdb86f99d0de2a9f419eed94fc5
Manujaya <manujaya.xyz@gmail.com>
2023-12-06 22:54:55 +0530

    Refactor carouselCard component to use Next.js Image component

--- app/components/carouselCard.jsx
+++ app/components/carouselCard.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { eventCardFormat } from '@/app/utility/formatDate'
+import Image from 'next/image'
 
 export default function CarouselCard(props) {
 
@@ -9,9 +10,7 @@ export default function CarouselCard(props) {
     const formattedDate = eventCardFormat(date)
     return (
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-            <img className="rounded-t-lg" src={imgPath} alt="" />
-        </a>
+         <Image className = "rounded-t-lg " src={imgPath} alt="header img" width= "390" height="200" priority ={true} />
         <div className="p-5">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
         <p className="text-sm text-gray-700 dark:text-gray-400">{society}</p>   
