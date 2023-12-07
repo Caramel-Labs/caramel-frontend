93069e6c7388fa97af35b84b57ca4bdf5628f5c8
Manujaya <manujaya.xyz@gmail.com>
2023-12-08 00:02:16 +0530

    Refactor event page and like button components

--- app/components/likeButton.jsx
+++ app/components/likeButton.jsx
@@ -1,15 +1,45 @@
 'use client'
 import { useState } from "react";
+import { useSession } from 'next-auth/react'
 
-export default function LikeButton(){
+
+export default function LikeButton(props) {
   const [liked, setLiked] = useState(false);
- const [likes, setLikes] = useState(100);
+  const [likes, setLikes] = useState(props.props.initialLikes);
+   console.log(props.props)
+  const { data: session } = useSession()
+  //const user = verifyJWT(session?.accessToken)
+  const currentUser = session?.user
 
   const heart = liked ? "❤️" : "♡";
-  const handleLike = () => {
-    
+  
+  const handleLike = async () => {
+
     setLikes(liked ? likes - 1 : likes + 1);
     setLiked(!liked);
+
+    try {
+      const userId = currentUser.username; // Replace with actual user ID
+      const eventId = props.props.eventId; // Replace with actual event ID
+      
+      const response = await fetch(`http://localhost:3001/events/${liked ? 'dislike' : 'like'}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ user_id: userId, event_id: eventId })
+      });
+
+      if (response.ok) {
+        // Update likes based on like/dislike action
+     console.log('Liked/disliked event')
+      } else {
+        // Handle error if the request fails
+        console.error('Failed to like/dislike event');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
@@ -17,6 +47,4 @@ export default function LikeButton(){
       {likes} {heart}
     </button>
   );
-};
-
-
+}
