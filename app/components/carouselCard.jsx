f40f20d92873eec74dc06fdad97fbe7ad8c1bbbb
Manujaya <manujaya.xyz@gmail.com>
2023-12-08 07:14:21 +0530

    Update image path in carouselCard component

--- app/components/carouselCard.jsx
+++ app/components/carouselCard.jsx
@@ -6,7 +6,7 @@ export default function CarouselCard(props) {
 
     const { _id, name, society, date } = props.event
     console.log(props.event)
-    const imgPath = `https://res.cloudinary.com/dekv3xmjm/image/upload/caramel/events/${_id}.jpg`
+    const imgPath = `https://res.cloudinary.com/dy3hecuzo/image/upload/v1701971490/Events/${name}.jpg`
     // const imgPath = `https://pbs.twimg.com/profile_images/1605279612216348672/PCYJMyaB_400x400.png`
     const formattedDate = eventCardFormat(date)
     return (
