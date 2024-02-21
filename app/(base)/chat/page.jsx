5960c0941435532fea656300c2c14fb68b1146e1
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2024-02-21 15:50:43 +0530

    UPDATE: resolve merge conflict + others

--- app/(base)/chat/page.jsx
+++ app/(base)/chat/page.jsx
@@ -9,7 +9,6 @@ export default function Chat() {
 	const [messages, setMessages] = useState([]);
 	const [formValue, setFormValue] = useState("");
 
-
 	const { data: session } = useSession();
 	const currentUser = session?.user;
 	const username = currentUser?.username;
@@ -30,37 +29,35 @@ export default function Chat() {
 	useEffect(() => {
 		const fetchMessages = async () => {
 			try {
-				const response = await fetch(`https://loop-backend.cyclic.app/chat/${username}`, {
-					method: "GET",
-					headers: {
-						"Content-Type": "application/json", // Set the appropriate content type if sending JSON data
-						// Add other headers if required
-					},
-				})
+				const response = await fetch(
+					`https://loop-backend.cyclic.app/chat/${username}`,
+					{
+						method: "GET",
+						headers: {
+							"Content-Type": "application/json", // Set the appropriate content type if sending JSON data
+							// Add other headers if required
+						},
+					}
+				);
 				const data = await response.json();
 
 				if (!response.ok) {
 					throw new Error("Network response was not ok");
-					
 				}
 				setMessages(data);
 				// dummy.current.scrollIntoView({ behavior: "smooth" });
-			
 			} catch (error) {
 				console.error("Error fetching messages:", error);
 			}
 		};
 
 		fetchMessages();
-		
-	}, [username,setMessages,dummy]);
+	}, [username, setMessages, dummy]);
 
 	useEffect(() => {
 		// Scroll to the bottom of the chat when the component mounts
 		dummy.current.scrollIntoView({ behavior: "auto" });
-	  }, []);
-	  
-
+	}, []);
 
 	const sendMessage = async (e) => {
 		e.preventDefault();
@@ -74,16 +71,15 @@ export default function Chat() {
 			// dummy.current.scrollIntoView({ behavior: "smooth" });
 			window.scrollTo({
 				top: dummy.current.offsetTop,
-				behavior: "smooth"
-			  });
+				behavior: "smooth",
+			});
 
 			const chatData = {
 				messege: formValue,
 				username: username,
-			}
-			
-			console.log(JSON.stringify(chatData))
+			};
 
+			console.log(JSON.stringify(chatData));
 
 			const response = await fetch("https://loop-backend.cyclic.app/chat/", {
 				method: "POST",
@@ -94,20 +90,18 @@ export default function Chat() {
 			});
 
 			const responseData = await response.json();
-			
-			 const botMessage = {
+
+			const botMessage = {
 				content: responseData.responseText,
 				isbot: true,
-			 }
-				// Update messages state with the received data if required
-				// For example:
-				setMessages((prevMessages) => [
-					...prevMessages,
-					botMessage, // Assuming 'responseData.message' holds the new message data
-				]);
-			
+			};
+			// Update messages state with the received data if required
+			// For example:
+			setMessages((prevMessages) => [
+				...prevMessages,
+				botMessage, // Assuming 'responseData.message' holds the new message data
+			]);
 
-<<<<<<< Updated upstream
 			if (!response.ok) {
 				throw new Error("Network response was not ok");
 			}
@@ -159,31 +153,4 @@ export default function Chat() {
 			</div>
 		</div>
 	);
-=======
-  {/* Input */}
-  <div className=" border-gray-200 px-4 mb-2 sm:mb-0 fixed left-0 right-0 mt-96 pt-72">
-    <form onSubmit={sendMessage} className="relative">
-      <div className="flex items-center">
-        <input
-          value={formValue}
-          onChange={(e) => setFormValue(e.target.value)}
-          placeholder="Say something nice"
-          type="text"
-          className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-4 bg-gray-200 rounded-md py-3 text-sm"
-        />
-        <button
-          type="submit"
-          disabled={!formValue}
-          className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white  hover:bg-none focus:outline-none absolute right-2 sm:relative sm:right-auto"
-        >
-          {sendIcon}
-        </button>
-      </div>
-    </form>
-  </div>
-</div>
-  
-  
-  );
->>>>>>> Stashed changes
 }
--- app/(base)/home/page.jsx
+++ app/(base)/home/page.jsx
@@ -16,13 +16,16 @@ export default function Home() {
 		console.log(currentUser, "current user");
 		const fetchEventData = async () => {
 			try {
-				const events = await fetch("https://loop-backend.cyclic.app/events/all", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({ faculty: `${currentUser.faculty}` }),
-				});
+				const events = await fetch(
+					"https://loop-backend.cyclic.app/events/all",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({ faculty: `${currentUser.faculty}` }),
+					}
+				);
 
 				const response = await events.json();
 				setEventData(response); // Update the state with the fetched data
@@ -31,30 +34,16 @@ export default function Home() {
 			}
 		};
 
-<<<<<<< Updated upstream
 		fetchEventData(); // Call the function to fetch event data
 	}, [currentUser]);
-=======
-    console.log(currentUser, "current user")
-    const fetchEventData = async () => {
-      try {
-        const events = await fetch('http://localhost:3001/events/all', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ faculty: `${currentUser.faculty}` })
-        });
->>>>>>> Stashed changes
 
 	//
 	return (
-		<div className="p-4 bg-zinc-950">
+		<div className="p-4 bg-zinc-950 pb-20">
 			<WelcomeHeader />
 
 			{/* search bar */}
 
-<<<<<<< Updated upstream
 			<div className="pt-2 relative mx-auto text-gray-600 flex flex-col mt-4  ">
 				<input
 					className="border-2 border-zinc-900 bg-zinc-900 h-12 px-5 pr-16 rounded-lg text-sm focus:outline-none text-xs "
@@ -80,19 +69,12 @@ export default function Home() {
 					</svg>
 				</button>
 			</div>
-=======
-
-
-  return (
-    <div className='p-5 bg-zinc-950' >
->>>>>>> Stashed changes
 
 			<div>
 				<h1 className="text-xl font-bold text-white my-6">You Might Like</h1>
 				<HomeCarousel />
 			</div>
 
-<<<<<<< Updated upstream
 			<div>
 				<h1 className="text-xl font-bold text-white mt-6">Happening Soon</h1>
 				{eventData &&
@@ -104,49 +86,4 @@ export default function Home() {
 			</div>
 		</div>
 	);
-=======
-      {/* search bar */}
-
-      <div className="pt-2 relative mx-auto text-gray-600 flex flex-col mt-4  ">
-        <input className="border-2 border-zinc-900 bg-zinc-900 h-12 px-5 pr-16 rounded-lg  focus:outline-none text-xs "
-          type="search" name="search" placeholder="Search for societies, events and more..." />
-        <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
-          <svg
-            className="text-gray-600 h-4 w-4 fill-current"
-            width="512"
-            height="512"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            version="1.1"
-            id="Capa_1"
-            x="0px"
-            y="0px"
-            viewBox="0 0 56.966 56.966"
-            xmlSpace="preserve"
-          >
-            <path
-              d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"
-            />
-          </svg>
-        </button>
-      </div>
-
-      <div>
-        <h1 className="text-xl font-bold text-white mt-6"> You Might Like</h1>
-        <HomeCarousel />
-      </div>
-
-      <div>
-        <h1 className="text-xl font-bold text-white mt-6"> Happening Soon</h1>
-        {eventData && eventData.map((event, i) => (
-          <Link key={i} href={`event/${event._id}`}>
-            <EventCard key={i} event={event} />
-          </Link>
-
-        ))}
-      </div>
-
-    </div>
-  )
->>>>>>> Stashed changes
 }
